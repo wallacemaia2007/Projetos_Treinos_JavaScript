ce238eaa789fbdd7f36e80f0df086301bb4867ea
Wallace Candido Maia Sousa <wallacemaia2007@gmail.com>
2025-05-09 00:24:51 -0300

    JQuery!

---
+++ JQuery/Exercicios/CaixaInterativa/CaixaInterativa.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    var contador = 0;
+
+    $("div").mouseenter(function(){
+        $("div#caixa").css("background", "green");
+    })
+
+    $("div#caixa").mouseleave(function () { 
+        $("div#caixa").css("background", "red");
+    });
+
+    $("div#caixa").click(function (e) { 
+        e.preventDefault();
+        $("div#caixa").css("background","blue");
+    });
+    $("div#caixa").dblclick(function (){
+        $("div#caixa").slideUp();
+        if(contador == 0){
+            $("#botao").after('<button id="botaoClique">Clique Aqui!</button>');
+            contador++;
+        }
+    });
+    $(document).on("click", "#botaoClique", function(){
+        $("div#caixa").fadeIn();
+    });
+});
